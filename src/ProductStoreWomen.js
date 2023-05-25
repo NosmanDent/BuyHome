76138ce1b3d8c16c9197624250f364d28936df3e
NosmanDent <olamiwale1212@gmail.com>
2023-05-26 03:24:43 +0800

    pic

--- src/ProductStoreWomen.js
+++ src/ProductStoreWomen.js
@@ -67,7 +67,7 @@ const productsArrayWomen = [
     title: "Zexe Bag",
     subTitle: "Lorem ipsym oplwe uwer seh seueo os",
     price: 569.99,
-    img: "../src/assets/women6.jpg",
+    img: "https://images.unsplash.com/photo-1617078262771-c2c39ac6867b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
     rating: "⭐⭐⭐⭐⭐",
   },
   {
